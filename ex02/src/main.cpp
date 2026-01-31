5417e8d46b391d34b287dfe741fed01a7c265284
n-goyat <Nathan.goyat@gmail.com>
2026-01-31 20:41:37 +0100

    added clarifications for stack manipulation

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -7,19 +7,19 @@ int main()
     // ===== MutantStack test =====
     MutantStack<int> mstack;
 
-    mstack.push(5);
-    mstack.push(17);
+    mstack.push(5); // [5]
+    mstack.push(17); // [5, 17]
 
     std::cout << "Top: " << mstack.top() << std::endl;
 
-    mstack.pop();
+    mstack.pop(); // [5]
 
     std::cout << "Size after pop: " << mstack.size() << std::endl;
 
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    mstack.push(0);
+    mstack.push(3); // [5, 3]
+    mstack.push(5); // [5, 3, 5]
+    mstack.push(737); // [5, 3, 5, 737]
+    mstack.push(0); // [5, 3, 5, 737, 0]
 
     // Iterate through the stack
     std::cout << "MutantStack contents:" << std::endl;
@@ -29,13 +29,13 @@ int main()
     // ===== Compare with list =====
     std::list<int> lst;
 
-    lst.push_back(5);
-    lst.push_back(17);
-    lst.pop_back();
-    lst.push_back(3);
-    lst.push_back(5);
-    lst.push_back(737);
-    lst.push_back(0);
+    lst.push_back(5); // [5]
+    lst.push_back(17); // [5, 17]
+    lst.pop_back(); // [5]
+    lst.push_back(3); // [5, 3]
+    lst.push_back(5); // [5, 3, 5]
+    lst.push_back(737); // [5, 3, 5, 737]
+    lst.push_back(0); // [5, 3, 5, 737, 0]
 
     std::cout << "\nList contents:" << std::endl;
     for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
